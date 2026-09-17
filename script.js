d747e9c76d665ea6632766437d4d430475a0470c
Ashanti <shamarkey03@yahoo.com>
2026-09-17 02:44:07 -0500

    add accesible html pages for the nav bar

---
+++ script.js
@@ -0,0 +1,19 @@
+let url = "https://student-data-api.ashantilemonia.workers.dev/api/v1/datasets/African-American-Inventors-and-Scientists/records?limit=10"
+
+let name = document.getElementById("outputName");
+let jobs = document.getElementById("outputOccupation");
+
+
+
+
+
+async function getinventors() {
+	const response = await fetch(url);
+	const data = await response.json();
+	console.log(data);
+	name.textContent = data.records[0].Name
+jobs.textContent = data.records[0]["Date of birth / death"]
+};
+
+getinventors();
+	
